5b4d1c0c88b5d0d153d052b2bd25a71bc23b30de
Your Name <your-email@domain.com>
2023-07-14 16:14:17 +0545

    Add types

---
+++ src/types/TUser.ts
@@ -0,0 +1,11 @@
+export interface TUser {
+  id?: number;
+  name?: string;
+  dob?: string;
+  email?: string;
+  password?: string;
+  googleToken?: string;
+  username?: string;
+  address?: string;
+  credential?: string;
+}
